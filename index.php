fb078cf6384e802ffe6c5e7567ff95f6f8c23c6f
Marcosmaciel2007 <79186614+Marcosmaciel2007@users.noreply.github.com>
2021-05-03 04:44:48 -0300

    1

--- index.php
+++ index.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>QuemTem+</title>
+    <title>QUEM TEM MAIS</title>
 </head>
 <body>
+
+<center>
     <form action="maior.php" method="post">
-        <label for="numeros">Insira uma lista do array associativos:</label><br>
+        <label for="numeros">COLOQUE AQUI UMA LISTA DE ARRAY ASSOCIATIVO:</label><br>
         <textarea name = "dados" id="numeros" rows="5" cols="30"></textarea><br>
         <input type="submit" value="enviar">
     </form>
+    </center>
+    
 </body>
 </html>
